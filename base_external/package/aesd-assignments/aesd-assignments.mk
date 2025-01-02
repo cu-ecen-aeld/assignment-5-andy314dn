2d9906fe30afb3e078b24bd2abd0abda1e844241
andy314dn-carbon <andy.314.dn@gmail.com>
2025-01-02 18:09:55 +0700

    add commit id for assignment 3

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,13 +6,17 @@
 ##############################################################
 
 # Reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'c84caeda45448937413158b33167d4e32f0b6263'
+AESD_ASSIGNMENTS_VERSION = '0cba5afe68d86a1476d5b86e13a001a25f2062e3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-andy314dn.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+# # In case we want to refer to Assignment 3 locally
+# AESD_ASSIGNMENTS_OVERRIDE_SRCDIR ?= <path_to_>/assignments-3-and-later-andy314dn
+# at Buildroot directory:
+# make aesd-assignments-rebuild
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
