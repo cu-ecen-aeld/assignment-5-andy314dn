42796bad076c04916d1fe557a228419f469d8494
andy314dn-carbon <andy.314.dn@gmail.com>
2025-10-25 15:02:41 +0700

    update install

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -5,10 +5,12 @@ LDD_SITE_METHOD = git
 # build the misc-modules kernel modules
 define LDD_BUILD_CMDS
     $(MAKE) -C $(@D)/misc-modules
+    $(MAKE) -C $(@D)/scull
 endef
 
-define LDD_INSTALL_CMDS
-	insmod $(@D)/misc-modules/hello.ko
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/misc-modules/hello.ko  $(TARGET_DIR)/lib/modules/$(LINUX_VERSION)
+    $(INSTALL) -m 0755 $(@D)/scull/scull.ko         $(TARGET_DIR)/lib/modules/$(LINUX_VERSION)
 endef
 
 $(eval $(generic-package))
