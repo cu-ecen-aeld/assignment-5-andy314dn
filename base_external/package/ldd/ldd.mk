4022d356a26c83ffda2bac869b8d832af0d15bd5
andy314dn-carbon <andy.314.dn@gmail.com>
2025-10-25 17:27:40 +0700

    add faulty module

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -16,8 +16,9 @@ endef
 
 # perform installation
 define LDD_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/misc-modules/hello.ko  $(TARGET_DIR)/lib/modules/$(LINUX_VERSION)
-    $(INSTALL) -m 0755 $(@D)/scull/scull.ko         $(TARGET_DIR)/lib/modules/$(LINUX_VERSION)
+	$(INSTALL) -m 0755 $(@D)/misc-modules/hello.ko   $(TARGET_DIR)/lib/modules/$(LINUX_VERSION)
+	$(INSTALL) -m 0755 $(@D)/misc-modules/faulty.ko  $(TARGET_DIR)/lib/modules/$(LINUX_VERSION)
+    $(INSTALL) -m 0755 $(@D)/scull/scull.ko          $(TARGET_DIR)/lib/modules/$(LINUX_VERSION)
 endef
 
 $(eval $(generic-package))
